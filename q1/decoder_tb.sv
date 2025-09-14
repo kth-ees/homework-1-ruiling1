6968ab1b5677b0373a1546129cb9090f7a69baa5
ruiling1 <ruiling_li@outlook.com>
2025-09-14 22:26:35 +0200

    Update decoder_tb.sv

--- q1/decoder_tb.sv
+++ q1/decoder_tb.sv
@@ -9,6 +9,14 @@ module decoder_tb;
     .binary(binary),
     .one_hot(one_hot)
   );
-
-  // Complete your testbench here
+ initial begin
+	for(int i=0;i<16;i++)begin
+		binary=i;
+		#10;
+		if(one_hot[binary]==1)begin
+			$display("The %d\t outpot is successful",i);
+		end
+	end
+ end
+ 
 endmodule
