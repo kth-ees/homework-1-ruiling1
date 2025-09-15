75c1a82540e57414a5080cd18effabfa858d1ab2
ruiling1 <ruiling_li@outlook.com>
2025-09-15 21:40:42 +0200

    Update count_1_tb.sv

--- q4/count_1_tb.sv
+++ q4/count_1_tb.sv
@@ -5,6 +5,20 @@ module count_1_tb;
 
   // Outputs
   logic [2:0] out;
-
-  // complete
+  count_1 uut(
+    .a(a),
+    .out(out)
+  );
+  initial begin
+    $display("Time |   a    | out | expected");
+    for (int i = 0; i < 16; i++) begin
+      a = i;             
+      #1;                  
+      $display("%4t | %04b |  %0d  |    %0d",
+                $time, a, out, $countones(a));
+      if (out !== $countones(a)) begin
+        $error("Mismatch! a=%b out=%0d expected=%0d", a, out, $countones(a));
+      end
+    end
+  end
 endmodule
